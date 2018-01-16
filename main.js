50ba05650bb0d8eda09c078a4a576b2186de5347
Angie <angiewashburn1997@gmail.com>
2018-01-16 11:21:09 -0500

    End of module 5

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
 $(document).ready(function() {
+   
+    $.ajax({
+        url: 'http://rest.learncode.academy/api/awashburn/friends',
+        type: 'Get'
+    }).done( (data) => {
+        data.forEach( (eachFriend) => {
+            let name = eachFriend.firstName + " " + eachFriend.lastName;
+            let newElement = createFriend(name);
+            $('#friend-list').append(newElement);
+        })
+        
+    }).fail( () => {
+        alert('AJAX call failed, unable to retrieve friends.');
+    })
+    
+    
     $("#friend-form").submit( (e) => {
         e.preventDefault();
         let newFriend = $('#input').val();
+        
+        //Split the name based on the first space we find
+        let firstName = newFriend.substr(0, newFriend.indexOf(' '));
+        let lastName = newFriend.substr(newFriend.indexOf(' ')+1);
+        //Construct the ajax request
+        $.ajax({
+            url: 'http://rest.learncode.academy/api/awashburn/friends',
+            type: 'POST',
+            data: {
+                firstName: firstName,
+                lastName: lastName,
+                email: `${firstName}@${lastName}.com`
+            }
+        
+    }).done( (data)  => {
+        console.log(data);
+    }).fail( () => {
+        alert('AJAX call failed, unable to POST new friend.');
+    })
         let newElement = createFriend(newFriend);
         $('#friend-list').append(newElement);
         $('#input').val("");
-    })
+});
 
     $('#friend-list').on('click', 'li', function(e) {
         $(this).remove();
