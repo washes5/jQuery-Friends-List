d10c821c78c07744cc0211b23ec8d66c24f98828
Angie <angiewashburn1997@gmail.com>
2018-01-16 09:42:49 -0500

    End of module 2

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    console.log("ready!");
+    $("#friend-form").submit( (e) => {
+        e.preventDefault();
+        let newFriend = $('#input').val();
+        let newElement = createFriend(newFriend);
+        $('#friend-list').append(newElement);
+        $('#input').val("");
+    })
+
+function createFriend(name) {
+    return $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+}
+
 });
